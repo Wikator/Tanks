ba0e1fe468361ae8718928ddc7e045e7248ab31c
Wikator <wiktor@szymulewicz.com>
2022-11-08 20:32:32 +0100

    Avatar testing

---
+++ Wii Tanks/Assets/Scripts/UI/LobbyPlayerTag.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Steamworks;
+using TMPro;
+
+public class LobbyPlayerTag : MonoBehaviour
+{
+    public TextMeshProUGUI playerNameText;
+
+    private bool avatarReceived;
+
+    public RawImage playerIcon;
+
+
+    protected Callback<AvatarImageLoaded_t> ImageLoaded;
+
+    private void Start()
+    {
+        ImageLoaded = Callback<AvatarImageLoaded_t>.Create(OnImageLoaded);
+    }
+
+    private void OnImageLoaded(AvatarImageLoaded_t callback)
+    {
+        if (callback.m_steamID.m_SteamID == PlayerNetworking.Instance.playerSteamID)
+        {
+            playerIcon.texture = GetSteamImageAsTexture(callback.m_iImage);
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    private Texture2D GetSteamImageAsTexture(int iImage)
+    {
+        Texture2D texture = null;
+
+        bool isValid = SteamUtils.GetImageSize(iImage, out uint width, out uint height);
+
+        if (isValid)
+        {
+            byte[] image = new byte[width * height * 4];
+
+            isValid = SteamUtils.GetImageRGBA(iImage, image, (int)(width * height* 4));
+
+            if (isValid)
+            {
+                texture = new Texture2D ((int)width, (int)height, TextureFormat.ARGB32, false, true);
+                texture.LoadRawTextureData(image);
+                texture.Apply();
+            }
+        }
+        avatarReceived = true;
+        return texture;
+    }
+
+    private void GetPlayerIcon()
+    {
+        int ImageID = SteamFriends.GetLargeFriendAvatar((CSteamID)PlayerNetworking.Instance.playerSteamID);
+
+        if (ImageID == -1)
+            return;
+
+        playerIcon.texture = GetSteamImageAsTexture(ImageID);
+    }
+
+    public void SetPlayerValues(PlayerNetworking player)
+    {
+        playerNameText.text = player.playerUsername;
+
+        if (!avatarReceived)
+            GetPlayerIcon();
+    }
+}
--- Wii Tanks/Assets/Scripts/UI/LobbyView.cs
+++ Wii Tanks/Assets/Scripts/UI/LobbyView.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 public abstract class LobbyView : View
 {
@@ -13,6 +15,13 @@ public abstract class LobbyView : View
 
     [SerializeField]
     protected Button startGameButton;
+
+    [SerializeField]
+    private GameObject viewContent;
+
+    private List<LobbyPlayerTag> playerTags = new();
+
+    
 
 
     //Each player will have to choose a color and tank type, thanks to the subclasses EliminationLobbyView and DeathmatchLobbyView
@@ -46,5 +55,67 @@ public abstract class LobbyView : View
         playersReadyCountText.text = "Players ready: " + Convert.ToString(GameManager.Instance.NumberOfReadyPlayers()) + "/" + Convert.ToString(GameManager.Instance.players.Count);
         chosenColorText.text = "Chosen color: " + PlayerNetworking.Instance.color;
         chosenTankTypeText.text = "Chosen tank type: " + PlayerNetworking.Instance.tankType;
+
+        UpdatePlayerList();
+    }
+
+    public void UpdatePlayerList()
+    {
+        if (playerTags.Count < GameManager.Instance.players.Count)
+        {
+            Debug.Log("test");
+            foreach (PlayerNetworking player in GameManager.Instance.players)
+            {
+                bool tagFound = false;
+                foreach (LobbyPlayerTag tag in playerTags)
+                {
+                    if (tag.playerNameText.text == player.playerUsername)
+                    {
+                        tagFound = true;
+                        break;
+                    }
+                }
+                if (!tagFound)
+                {
+                    GameObject newPlayerTag = Instantiate(Addressables.LoadAssetAsync<GameObject>("PlayerTag").WaitForCompletion());
+                    LobbyPlayerTag newPlayerTagScript = newPlayerTag.GetComponent<LobbyPlayerTag>();
+
+                    newPlayerTagScript.SetPlayerValues(player);
+
+                    newPlayerTag.transform.SetParent(viewContent.transform);
+                    newPlayerTag.transform.localEulerAngles = Vector3.one;
+
+                    playerTags.Add(newPlayerTagScript);
+                }
+                
+            }
+        }
+
+        if (playerTags.Count > GameManager.Instance.players.Count)
+        {
+            foreach (LobbyPlayerTag tag in playerTags)
+            {
+                bool playerFound = false;
+                foreach (PlayerNetworking player in GameManager.Instance.players)
+                {
+                    if (tag.playerNameText.text == player.playerUsername)
+                    {
+                        playerFound = true;
+                        break;
+                    }
+                }
+                if (!playerFound)
+                {
+                    playerTags.Remove(tag);
+                    Destroy(tag);
+                }
+                
+            }
+        }
+
+        if (playerTags.Count == GameManager.Instance.players.Count)
+        {
+
+        }
     }
 }
